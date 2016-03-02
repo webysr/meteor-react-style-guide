9192e6603f7ac0183408a8509863292b47507eca
Jürgen Schlieber <juergen.schlieber@gmail.com>
2016-03-02 16:45:55 +0100

    Remove reactTapEventPlugin

--- client/styleGuide.js
+++ client/styleGuide.js
@@ -9,12 +9,6 @@ export default class ReactStyleGuide {
   }
 
   init() {
-    // Needed for onTouchTap
-    // Can go away when react 1.0 release
-    // Check this repo:
-    // https://github.com/zilverline/react-tap-event-plugin
-    reactTapEventPlugin();
-
     console.log('Auto generating style guide...');
     this.router = new StyleGuideRouter();
   }
