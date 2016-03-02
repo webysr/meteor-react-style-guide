7e47618c7ad4086598da96d915614a8e964bc2da
Jürgen Schlieber <juergen.schlieber@gmail.com>
2016-03-02 18:47:58 +0100

    Fix version constraints

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'webysr:react-style-guide',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'An auto generated style guide for your react components using meteor and flow router',
   git: 'https://github.com/webysr/react-style-guide',
   documentation: 'README.md'
@@ -8,10 +8,9 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3-beta.11');
-  api.use('ecmascript');
-  api.use('mongo');
-  api.use('react-meteor-data');
-
+  api.use('ecmascript@0.4.0-beta.11');
+  api.use('mongo@1.1.4-beta.11');
+  api.use('react-meteor-data@0.2.5');
   api.use('meteorhacks:flow-router@1.17.2', 'client', {weak: true});
   api.use('kadira:flow-router@2.6.0', 'client', {weak: true});
 
